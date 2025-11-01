fd1e9715f9da10276855f03cb4dba4d786c7af6b
Niall Murray <narwhalegamesyt@gmail.com>
2025-11-01 11:50:55 -0700

    cleaning

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,6 +1,7 @@
 let slide_index = 1;
 showSlide(slide_index);
 
+// user controls - shift slide left or right
 function shiftSlide(n) {
     console.log(slide_index);
     showSlide(slide_index += n);
@@ -16,14 +17,16 @@ function showSlide(n) {
     // loop back to the start
     if(n > slides.length)
         slide_index = 1;
+
     // loop back to the end
     if(n < 1)
         slide_index = slides.length
 
-    let i;
-    for(i = 0; i < slides.length; i++) {
+    // set all slides to invisible
+    for(let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+    // set the currently selected slide to visible
     slides[slide_index - 1].style.display = "block";
 }
