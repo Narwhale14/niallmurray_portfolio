f138803e12d94b7c7d97c5ef8a3a26955d5d5858
Niall Murray <narwhalegamesyt@gmail.com>
2025-11-01 23:04:03 -0700

    full functioning dark mode

--- JS/buttons.js
+++ JS/buttons.js
@@ -6,14 +6,19 @@ document.getElementById("jump-to-portfolio").addEventListener("click", () => {
 // Dark mode switch
 const applicable_bg = document.querySelectorAll(".dark-mode-bg");
 const applicable_txt = document.querySelectorAll(".dark-mode-text");
-const applicable_img = document.querySelectorAll(".dark-mode-img")
+const applicable_img = document.querySelectorAll(".dark-mode-img");
+const applicable_pan = document.querySelectorAll(".panel");
+const intro_card = document.getElementById("intro-card");
 const toggle = document.getElementById("switch");
 const knob = document.getElementById("knob");
 
 toggle.addEventListener("click", () => {
-    knob.classList.toggle("darkMode");
+    knob.classList.toggle("dark-mode");
+    toggle.classList.toggle("dark-mode");
+    intro_card.classList.toggle("dark-mode");
     applicable_bg.forEach(element => element.classList.toggle("active"));
     applicable_txt.forEach(element => element.classList.toggle("active"));
+    applicable_pan.forEach(element => element.classList.toggle("dark-mode"));
 
     applicable_img.forEach(element => {
         // Fades out, swaps, fades back in
