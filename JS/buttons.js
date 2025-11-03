848e259de13d69ee231aa4055b7dd98aa38d3170
Niall Murray <narwhalegamesyt@gmail.com>
2025-11-02 19:53:46 -0800

    section headers

--- JS/buttons.js
+++ JS/buttons.js
@@ -3,6 +3,16 @@ document.getElementById("jump-to-portfolio").addEventListener("click", () => {
     document.getElementById("recieve-jump-portfolio").scrollIntoView({ behavior: "smooth" });
 });
 
+// Bio button
+document.getElementById("jump-to-bio").addEventListener("click", () => {
+    document.getElementById("recieve-jump-bio").scrollIntoView({ behavior: "smooth" });
+});
+
+// Contact button
+document.getElementById("jump-to-contact").addEventListener("click", () => {
+    document.getElementById("recieve-jump-contact").scrollIntoView({ behavior: "smooth" });
+});
+
 // Dark mode switch
 const applicable_bg = document.querySelectorAll(".dark-mode-bg");
 const applicable_txt = document.querySelectorAll(".dark-mode-text");
